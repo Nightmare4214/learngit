312b677d0804f71129669f3ac708ba24d2fc408f
Nightmare4214 <37721929+Nightmare4214@users.noreply.github.com>
2018-10-29 09:20:49 +0800

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.result = new System.Windows.Forms.Label();
+            this.Seven = new System.Windows.Forms.Button();
+            this.Eight = new System.Windows.Forms.Button();
+            this.Nine = new System.Windows.Forms.Button();
+            this.Four = new System.Windows.Forms.Button();
+            this.Five = new System.Windows.Forms.Button();
+            this.Six = new System.Windows.Forms.Button();
+            this.One = new System.Windows.Forms.Button();
+            this.Two = new System.Windows.Forms.Button();
+            this.Three = new System.Windows.Forms.Button();
+            this.Zero = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.Divid = new System.Windows.Forms.Button();
+            this.Multiply = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.Plus = new System.Windows.Forms.Button();
+            this.Equal = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.result.Location = new System.Drawing.Point(29, 21);
+            this.result.Name = "result";
+            this.result.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.result.Size = new System.Drawing.Size(597, 118);
+            this.result.TabIndex = 0;
+            this.result.Text = "0";
+            // 
+            // Seven
+            // 
+            this.Seven.Location = new System.Drawing.Point(32, 154);
+            this.Seven.Name = "Seven";
+            this.Seven.Size = new System.Drawing.Size(80, 80);
+            this.Seven.TabIndex = 1;
+            this.Seven.Text = "7";
+            this.Seven.UseVisualStyleBackColor = true;
+            this.Seven.Click += new System.EventHandler(this.Num_Click);
+            // 
+            // Eight
+            // 
+            this.Eight.Location = new System.Drawing.Point(165, 154);
+            this.Eight.Name = "Eight";
+            this.Eight.Size = new System.Drawing.Size(80, 80);
+            this.Eight.TabIndex = 2;
+            this.Eight.Text = "8";
+            this.Eight.UseVisualStyleBackColor = true;
+            this.Eight.Click += new System.EventHandler(this.Num_Click);
+            // 
+            // Nine
+            // 
+            this.Nine.Location = new System.Drawing.Point(300, 154);
+            this.Nine.Name = "Nine";
+            this.Nine.Size = new System.Drawing.Size(80, 80);
+            this.Nine.TabIndex = 3;
+            this.Nine.Text = "9";
+            this.Nine.UseVisualStyleBackColor = true;
+            this.Nine.Click += new System.EventHandler(this.Num_Click);
+            // 
+            // Four
+            // 
+            this.Four.Location = new System.Drawing.Point(32, 275);
+            this.Four.Name = "Four";
+            this.Four.Size = new System.Drawing.Size(80, 80);
+            this.Four.TabIndex = 4;
+            this.Four.Text = "4";
+            this.Four.UseVisualStyleBackColor = true;
+            this.Four.Click += new System.EventHandler(this.Num_Click);
+            // 
+            // Five
+            // 
+            this.Five.Location = new System.Drawing.Point(165, 275);
+            this.Five.Name = "Five";
+            this.Five.Size = new System.Drawing.Size(80, 80);
+            this.Five.TabIndex = 5;
+            this.Five.Text = "5";
+            this.Five.UseVisualStyleBackColor = true;
+            this.Five.Click += new System.EventHandler(this.Num_Click);
+            // 
+            // Six
+            // 
+            this.Six.Location = new System.Drawing.Point(300, 275);
+            this.Six.Name = "Six";
+            this.Six.Size = new System.Drawing.Size(80, 80);
+            this.Six.TabIndex = 6;
+            this.Six.Text = "6";
+            this.Six.UseVisualStyleBackColor = true;
+            this.Six.Click += new System.EventHandler(this.Num_Click);
+            // 
+            // One
+            // 
+            this.One.Location = new System.Drawing.Point(32, 393);
+            this.One.Name = "One";
+            this.One.Size = new System.Drawing.Size(80, 80);
+            this.One.TabIndex = 7;
+            this.One.Text = "1";
+            this.One.UseVisualStyleBackColor = true;
+            this.One.Click += new System.EventHandler(this.Num_Click);
+            // 
+            // Two
+            // 
+            this.Two.Location = new System.Drawing.Point(165, 393);
+            this.Two.Name = "Two";
+            this.Two.Size = new System.Drawing.Size(80, 80);
+            this.Two.TabIndex = 8;
+            this.Two.Text = "2";
+            this.Two.UseVisualStyleBackColor = true;
+            this.Two.Click += new System.EventHandler(this.Num_Click);
+            // 
+            // Three
+            // 
+            this.Three.Location = new System.Drawing.Point(300, 393);
+            this.Three.Name = "Three";
+            this.Three.Size = new System.Drawing.Size(80, 80);
+            this.Three.TabIndex = 9;
+            this.Three.Text = "3";
+            this.Three.UseVisualStyleBackColor = true;
+            this.Three.Click += new System.EventHandler(this.Num_Click);
+            // 
+            // Zero
+            // 
+            this.Zero.Location = new System.Drawing.Point(32, 504);
+            this.Zero.Name = "Zero";
+            this.Zero.Size = new System.Drawing.Size(213, 80);
+            this.Zero.TabIndex = 10;
+            this.Zero.Text = "0";
+            this.Zero.UseVisualStyleBackColor = true;
+            this.Zero.Click += new System.EventHandler(this.Num_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(300, 504);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(80, 80);
+            this.button11.TabIndex = 11;
+            this.button11.Text = ".";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // Divid
+            // 
+            this.Divid.Location = new System.Drawing.Point(421, 154);
+            this.Divid.Name = "Divid";
+            this.Divid.Size = new System.Drawing.Size(80, 80);
+            this.Divid.TabIndex = 12;
+            this.Divid.Text = "/";
+            this.Divid.UseVisualStyleBackColor = true;
+            // 
+            // Multiply
+            // 
+            this.Multiply.Location = new System.Drawing.Point(421, 275);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(80, 80);
+            this.Multiply.TabIndex = 13;
+            this.Multiply.Text = "*";
+            this.Multiply.UseVisualStyleBackColor = true;
+            // 
+            // Minus
+            // 
+            this.Minus.Location = new System.Drawing.Point(421, 393);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(80, 80);
+            this.Minus.TabIndex = 14;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = true;
+            // 
+            // Plus
+            // 
+            this.Plus.Location = new System.Drawing.Point(421, 504);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(80, 80);
+            this.Plus.TabIndex = 15;
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = true;
+            // 
+            // Equal
+            // 
+            this.Equal.Location = new System.Drawing.Point(544, 154);
+            this.Equal.Name = "Equal";
+            this.Equal.Size = new System.Drawing.Size(82, 430);
+            this.Equal.TabIndex = 16;
+            this.Equal.Text = "=";
+            this.Equal.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 629);
+            this.Controls.Add(this.Equal);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.Divid);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.Zero);
+            this.Controls.Add(this.Three);
+            this.Controls.Add(this.Two);
+            this.Controls.Add(this.One);
+            this.Controls.Add(this.Six);
+            this.Controls.Add(this.Five);
+            this.Controls.Add(this.Four);
+            this.Controls.Add(this.Nine);
+            this.Controls.Add(this.Eight);
+            this.Controls.Add(this.Seven);
+            this.Controls.Add(this.result);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button Seven;
+        private System.Windows.Forms.Button Eight;
+        private System.Windows.Forms.Button Nine;
+        private System.Windows.Forms.Button Four;
+        private System.Windows.Forms.Button Five;
+        private System.Windows.Forms.Button Six;
+        private System.Windows.Forms.Button One;
+        private System.Windows.Forms.Button Two;
+        private System.Windows.Forms.Button Three;
+        private System.Windows.Forms.Button Zero;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button Divid;
+        private System.Windows.Forms.Button Multiply;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Button Equal;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        bool clear;
+        public Form1()
+        {
+            InitializeComponent();
+            clear = false;
+        }
+        private void doClear()
+        {
+            result.Text = "0";
+            clear = false;
+        }
+        private int getPos()
+        {
+            string text = result.Text;
+            int pos = text.Length - 1;
+            for(int i = text.Length - 1; i >= 0; --i)
+            {
+                if (char.IsNumber(text[i]))
+                {
+                    pos = i;
+                }
+                else
+                {
+                    return pos;
+                }
+            }
+            return 0;
+        }
+        private int findFirstNotOf(string text,char target)
+        {
+            for(int i = 0; i < text.Length; ++i)
+            {
+                if (text[i] != target)
+                {
+                    return i;
+                }
+            }
+            return -1;
+
+        }
+        private void Num_Click(object sender, EventArgs e)
+        {
+            if (clear)
+            {
+                doClear();
+            }
+            string num = ((Button)sender).Text;
+            string text = result.Text;
+            string op = text.Substring(getPos());
+            //操作符或者小数点结尾
+            if(!char.IsNumber(text[text.Length-1])&& text[text.Length - 1] != '.')
+            {
+                result.Text += num;
+            }
+            else
+            {
+                //前缀0不加0
+                if (findFirstNotOf(op, '0') == -1 && num == "0") return;
+                
+                if (op == "0") result.Text = text.Substring(0, text.Length - 1) + num;
+                else result.Text += num;
+            }
+            
+        }
+    }
+}
