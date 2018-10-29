9d4c5d121f69b4b6a9d048376267ed2fab6a75ee
Nightmare <2499015249@qq.com>
2018-10-29 13:49:05 +0800

    Revert "add Test"
    This reverts commit b7e8ec2723b8dff986f87b5dd18455ab12d88c6d.

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -49,19 +49,22 @@
             // 
             // result
             // 
+            this.result.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.result.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.result.Location = new System.Drawing.Point(29, 21);
+            this.result.Location = new System.Drawing.Point(22, 17);
+            this.result.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.result.Name = "result";
-            this.result.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.result.Size = new System.Drawing.Size(597, 118);
+            this.result.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.result.Size = new System.Drawing.Size(448, 94);
             this.result.TabIndex = 0;
             this.result.Text = "0";
             // 
             // Seven
             // 
-            this.Seven.Location = new System.Drawing.Point(32, 154);
+            this.Seven.Location = new System.Drawing.Point(24, 123);
+            this.Seven.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Seven.Name = "Seven";
-            this.Seven.Size = new System.Drawing.Size(80, 80);
+            this.Seven.Size = new System.Drawing.Size(60, 64);
             this.Seven.TabIndex = 1;
             this.Seven.Text = "7";
             this.Seven.UseVisualStyleBackColor = true;
@@ -69,9 +72,10 @@
             // 
             // Eight
             // 
-            this.Eight.Location = new System.Drawing.Point(165, 154);
+            this.Eight.Location = new System.Drawing.Point(124, 123);
+            this.Eight.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Eight.Name = "Eight";
-            this.Eight.Size = new System.Drawing.Size(80, 80);
+            this.Eight.Size = new System.Drawing.Size(60, 64);
             this.Eight.TabIndex = 2;
             this.Eight.Text = "8";
             this.Eight.UseVisualStyleBackColor = true;
@@ -79,9 +83,10 @@
             // 
             // Nine
             // 
-            this.Nine.Location = new System.Drawing.Point(300, 154);
+            this.Nine.Location = new System.Drawing.Point(225, 123);
+            this.Nine.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Nine.Name = "Nine";
-            this.Nine.Size = new System.Drawing.Size(80, 80);
+            this.Nine.Size = new System.Drawing.Size(60, 64);
             this.Nine.TabIndex = 3;
             this.Nine.Text = "9";
             this.Nine.UseVisualStyleBackColor = true;
@@ -89,9 +94,10 @@
             // 
             // Four
             // 
-            this.Four.Location = new System.Drawing.Point(32, 275);
+            this.Four.Location = new System.Drawing.Point(24, 220);
+            this.Four.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Four.Name = "Four";
-            this.Four.Size = new System.Drawing.Size(80, 80);
+            this.Four.Size = new System.Drawing.Size(60, 64);
             this.Four.TabIndex = 4;
             this.Four.Text = "4";
             this.Four.UseVisualStyleBackColor = true;
@@ -99,9 +105,10 @@
             // 
             // Five
             // 
-            this.Five.Location = new System.Drawing.Point(165, 275);
+            this.Five.Location = new System.Drawing.Point(124, 220);
+            this.Five.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Five.Name = "Five";
-            this.Five.Size = new System.Drawing.Size(80, 80);
+            this.Five.Size = new System.Drawing.Size(60, 64);
             this.Five.TabIndex = 5;
             this.Five.Text = "5";
             this.Five.UseVisualStyleBackColor = true;
@@ -109,9 +116,10 @@
             // 
             // Six
             // 
-            this.Six.Location = new System.Drawing.Point(300, 275);
+            this.Six.Location = new System.Drawing.Point(225, 220);
+            this.Six.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Six.Name = "Six";
-            this.Six.Size = new System.Drawing.Size(80, 80);
+            this.Six.Size = new System.Drawing.Size(60, 64);
             this.Six.TabIndex = 6;
             this.Six.Text = "6";
             this.Six.UseVisualStyleBackColor = true;
@@ -119,9 +127,10 @@
             // 
             // One
             // 
-            this.One.Location = new System.Drawing.Point(32, 393);
+            this.One.Location = new System.Drawing.Point(24, 314);
+            this.One.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.One.Name = "One";
-            this.One.Size = new System.Drawing.Size(80, 80);
+            this.One.Size = new System.Drawing.Size(60, 64);
             this.One.TabIndex = 7;
             this.One.Text = "1";
             this.One.UseVisualStyleBackColor = true;
@@ -129,9 +138,10 @@
             // 
             // Two
             // 
-            this.Two.Location = new System.Drawing.Point(165, 393);
+            this.Two.Location = new System.Drawing.Point(124, 314);
+            this.Two.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Two.Name = "Two";
-            this.Two.Size = new System.Drawing.Size(80, 80);
+            this.Two.Size = new System.Drawing.Size(60, 64);
             this.Two.TabIndex = 8;
             this.Two.Text = "2";
             this.Two.UseVisualStyleBackColor = true;
@@ -139,9 +149,10 @@
             // 
             // Three
             // 
-            this.Three.Location = new System.Drawing.Point(300, 393);
+            this.Three.Location = new System.Drawing.Point(225, 314);
+            this.Three.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Three.Name = "Three";
-            this.Three.Size = new System.Drawing.Size(80, 80);
+            this.Three.Size = new System.Drawing.Size(60, 64);
             this.Three.TabIndex = 9;
             this.Three.Text = "3";
             this.Three.UseVisualStyleBackColor = true;
@@ -149,9 +160,10 @@
             // 
             // Zero
             // 
-            this.Zero.Location = new System.Drawing.Point(32, 504);
+            this.Zero.Location = new System.Drawing.Point(24, 403);
+            this.Zero.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Zero.Name = "Zero";
-            this.Zero.Size = new System.Drawing.Size(213, 80);
+            this.Zero.Size = new System.Drawing.Size(160, 64);
             this.Zero.TabIndex = 10;
             this.Zero.Text = "0";
             this.Zero.UseVisualStyleBackColor = true;
@@ -159,63 +171,75 @@
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(300, 504);
+            this.button11.Location = new System.Drawing.Point(225, 403);
+            this.button11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(80, 80);
+            this.button11.Size = new System.Drawing.Size(60, 64);
             this.button11.TabIndex = 11;
             this.button11.Text = ".";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.Point_Click);
             // 
             // Divid
             // 
-            this.Divid.Location = new System.Drawing.Point(421, 154);
+            this.Divid.Location = new System.Drawing.Point(316, 123);
+            this.Divid.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Divid.Name = "Divid";
-            this.Divid.Size = new System.Drawing.Size(80, 80);
+            this.Divid.Size = new System.Drawing.Size(60, 64);
             this.Divid.TabIndex = 12;
             this.Divid.Text = "/";
             this.Divid.UseVisualStyleBackColor = true;
+            this.Divid.Click += new System.EventHandler(this.Operation_Click);
             // 
             // Multiply
             // 
-            this.Multiply.Location = new System.Drawing.Point(421, 275);
+            this.Multiply.Location = new System.Drawing.Point(316, 220);
+            this.Multiply.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Multiply.Name = "Multiply";
-            this.Multiply.Size = new System.Drawing.Size(80, 80);
+            this.Multiply.Size = new System.Drawing.Size(60, 64);
             this.Multiply.TabIndex = 13;
             this.Multiply.Text = "*";
             this.Multiply.UseVisualStyleBackColor = true;
+            this.Multiply.Click += new System.EventHandler(this.Operation_Click);
             // 
             // Minus
             // 
-            this.Minus.Location = new System.Drawing.Point(421, 393);
+            this.Minus.Location = new System.Drawing.Point(316, 314);
+            this.Minus.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Minus.Name = "Minus";
-            this.Minus.Size = new System.Drawing.Size(80, 80);
+            this.Minus.Size = new System.Drawing.Size(60, 64);
             this.Minus.TabIndex = 14;
             this.Minus.Text = "-";
             this.Minus.UseVisualStyleBackColor = true;
+            this.Minus.Click += new System.EventHandler(this.Operation_Click);
             // 
             // Plus
             // 
-            this.Plus.Location = new System.Drawing.Point(421, 504);
+            this.Plus.Location = new System.Drawing.Point(316, 403);
+            this.Plus.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Plus.Name = "Plus";
-            this.Plus.Size = new System.Drawing.Size(80, 80);
+            this.Plus.Size = new System.Drawing.Size(60, 64);
             this.Plus.TabIndex = 15;
             this.Plus.Text = "+";
             this.Plus.UseVisualStyleBackColor = true;
+            this.Plus.Click += new System.EventHandler(this.Operation_Click);
             // 
             // Equal
             // 
-            this.Equal.Location = new System.Drawing.Point(544, 154);
+            this.Equal.Location = new System.Drawing.Point(408, 123);
+            this.Equal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Equal.Name = "Equal";
-            this.Equal.Size = new System.Drawing.Size(82, 430);
+            this.Equal.Size = new System.Drawing.Size(62, 344);
             this.Equal.TabIndex = 16;
             this.Equal.Text = "=";
             this.Equal.UseVisualStyleBackColor = true;
+            this.Equal.Click += new System.EventHandler(this.Equal_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(682, 629);
+            this.ClientSize = new System.Drawing.Size(512, 503);
             this.Controls.Add(this.Equal);
             this.Controls.Add(this.Plus);
             this.Controls.Add(this.Minus);
@@ -233,6 +257,7 @@
             this.Controls.Add(this.Eight);
             this.Controls.Add(this.Seven);
             this.Controls.Add(this.result);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -18,9 +18,95 @@ namespace WindowsFormsApp1
             InitializeComponent();
             clear = false;
         }
+        private int priority(char c)
+        {
+            switch (c)
+            {
+                case '*':
+                case '/':
+                    return 3;
+                case '+':
+                case '-':
+                    return 2;
+                case '#':
+                    return 1;
+                default:
+                    return 0;
+            }
+
+        }
+        private bool compare(char a,char b)
+        {
+            return priority(a) > priority(b);
+        }
+        private double calculate(double a,double b,char c)
+        {
+            switch (c)
+            {
+                case '+':
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '*':
+                    return a * b;
+                case '/':
+                    if (b == 0) throw new Exception("divid by zero");
+                    return a / b;
+                default:
+                    throw new Exception("wrong express");
+            }
+        }
+        //中缀表达式求值
+        private string getResult(string express)
+        {
+            char lastChar = express[express.Length - 1];
+            if (!char.IsNumber(lastChar)&&lastChar!='.')
+            {
+                express=express.Substring(0, express.Length - 1);
+            }
+            try {
+                Stack<char> operation = new Stack<char>();
+                operation.Push('#');
+                Stack<double> num = new Stack<double>();
+                for (int i = 0; i < express.Length; ++i)
+                {
+                    string temp = "";
+                    while (i<express.Length&&(char.IsNumber(express[i]) || express[i] == '.'))
+                    {
+                        temp += express[i];
+                        ++i;
+                    }
+                    num.Push(Convert.ToDouble(temp));
+                    if (i == express.Length) break;
+                    char op = express[i];
+                    while (!compare(op, operation.Peek()))
+                    {
+                        double b = num.Peek();
+                        num.Pop();
+                        double a = num.Peek();
+                        num.Pop();
+                        num.Push(calculate(a, b, operation.Pop()));
+                    }
+                    operation.Push(op);
+                }
+                while (operation.Peek() != '#')
+                {
+                    double b = num.Peek();
+                    num.Pop();
+                    double a = num.Peek();
+                    num.Pop();
+                    num.Push(calculate(a, b, operation.Pop()));
+                }
+                return Convert.ToString(num.Peek());
+            }
+            catch(Exception e)
+            {
+                return e.Message;
+            }
+        }
         private void doClear()
         {
-            result.Text = "0";
+            if(clear)result.Text = "0";
             clear = false;
         }
         private int getPos()
@@ -29,7 +115,7 @@ namespace WindowsFormsApp1
             int pos = text.Length - 1;
             for(int i = text.Length - 1; i >= 0; --i)
             {
-                if (char.IsNumber(text[i]))
+                if (char.IsNumber(text[i])||text[i]=='.')
                 {
                     pos = i;
                 }
@@ -52,12 +138,17 @@ namespace WindowsFormsApp1
             return -1;
 
         }
+        //返回最后一个操作数，如果最后是操作符，则返回空
+        private string getLastOperator()
+        {
+            string text = result.Text;
+            string temp = text.Substring(getPos());
+            if (!char.IsNumber(temp[0]) && temp[0] != '.') return "";
+            else return temp;
+        }
         private void Num_Click(object sender, EventArgs e)
         {
-            if (clear)
-            {
-                doClear();
-            }
+            doClear();
             string num = ((Button)sender).Text;
             string text = result.Text;
             string op = text.Substring(getPos());
@@ -75,6 +166,35 @@ namespace WindowsFormsApp1
                 else result.Text += num;
             }
             
+        }
+        private void Point_Click(object sender, EventArgs e)
+        {
+            doClear();
+            string text = result.Text;
+            string op = getLastOperator();
+            if (op == "") result.Text += "0.";
+            else if (op.IndexOf('.') != -1) return;
+            else result.Text = result.Text + '.';
+        }
+        private void Operation_Click(object sender,EventArgs e)
+        {
+            doClear();
+            string text = result.Text;
+            string operation = ((Button)sender).Text;
+            char lastChar = text[text.Length - 1];
+            if (char.IsNumber(lastChar))
+            {
+                result.Text += operation;
+            }else
+            {
+                result.Text = result.Text.Substring(0, text.Length - 1) + operation;
+            }
+        }
+        private void Equal_Click(object sender,EventArgs e)
+        {
+            string text = result.Text;
+            result.Text = getResult(text);
+            clear = true;
         }
     }
 }
